2d69bd2d72ccac64cfb231c69a06cf44d940e74c
technicise.com <info@technicise.com>
2013-09-26 20:06:05 +0530

    adding linux and web code

---
+++ code/FileManagement/FMSClassLibrary/DBinteraction.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FMSClassLibrary
+{
+
+    public class DbInteraction
+    {
+        static string passwordCurrent = "technicise";
+        static string dbmsCurrent = "FMSdb";
+
+        private static MySql.Data.MySqlClient.MySqlConnection OpenDbConnection()
+        {
+            MySql.Data.MySqlClient.MySqlConnection msqlConnection = null;
+
+            msqlConnection = new MySql.Data.MySqlClient.MySqlConnection("server=localhost;user id=root;Password=" + passwordCurrent + ";database=" + dbmsCurrent + ";persist security info=False");
+
+            //open the connection
+            if (msqlConnection.State != System.Data.ConnectionState.Open)
+                msqlConnection.Open();
+
+            return msqlConnection;
+        }
+
+        #region User
+
+        public static int DoRegisterNewUser(UserInfo NewUser)
+        {
+            return DoRegisterNewuserindb(NewUser);
+        }
+
+        private static int DoRegisterNewuserindb(UserInfo NewUser)
+        {
+            int returnVal = 0;
+            MySql.Data.MySqlClient.MySqlConnection msqlConnection = OpenDbConnection();
+
+            try
+            {
+                //define the command reference
+                MySql.Data.MySqlClient.MySqlCommand msqlCommand = new MySql.Data.MySqlClient.MySqlCommand();
+
+                //define the connection used by the command object
+                msqlCommand.Connection = msqlConnection;
+
+                msqlCommand.CommandText = "INSERT INTO user(id,userid,passwrd,hints) " + "VALUES(@id,@userid,@passwrd,@hints)";
+
+                msqlCommand.Parameters.AddWithValue("@id", NewUser.userId);
+                msqlCommand.Parameters.AddWithValue("@userid", NewUser.userId);
+                msqlCommand.Parameters.AddWithValue("@passwrd", NewUser.userName);
+                msqlCommand.Parameters.AddWithValue("@hints", NewUser.deviceOwned);
+
+
+                msqlCommand.ExecuteNonQuery();
+
+                returnVal = 1;
+            }
+            catch (Exception er)
+            {
+                returnVal = 0;
+            }
+            finally
+            {
+                //always close the connection
+                msqlConnection.Close();
+            }
+            return returnVal;
+        }
+
+        #endregion
+       
+        #region ID password
+
+        public static string FetcheId()
+        {
+
+            string idStr = string.Empty;
+
+            int returnVal = 0;
+            MySql.Data.MySqlClient.MySqlConnection msqlConnection = OpenDbConnection();
+
+            try
+            {
+
+
+                //define the command reference
+                MySql.Data.MySqlClient.MySqlCommand msqlCommand = new MySql.Data.MySqlClient.MySqlCommand();
+
+                //define the connection used by the command object
+                msqlCommand.Connection = msqlConnection;
+
+
+                msqlCommand.CommandText = "Select userid from user;";
+                MySql.Data.MySqlClient.MySqlDataReader msqlReader = msqlCommand.ExecuteReader();
+
+                msqlReader.Read();
+
+                idStr = msqlReader.GetString("userid");
+
+            }
+            catch (Exception er)
+            {
+                //Assert//.Show(er.Message);
+            }
+            finally
+            {
+                //always close the connection
+                msqlConnection.Close();
+            }
+
+            return idStr;
+        }
+
+        public static string FetchePassword()
+        {
+
+            string passwordStr = string.Empty;
+
+            int returnVal = 0;
+            MySql.Data.MySqlClient.MySqlConnection msqlConnection = OpenDbConnection();
+
+            try
+            {
+
+
+                //define the command reference
+                MySql.Data.MySqlClient.MySqlCommand msqlCommand = new MySql.Data.MySqlClient.MySqlCommand();
+
+                //define the connection used by the command object
+                msqlCommand.Connection = msqlConnection;
+
+
+                msqlCommand.CommandText = "Select passwrd from user;";
+                MySql.Data.MySqlClient.MySqlDataReader msqlReader = msqlCommand.ExecuteReader();
+
+                msqlReader.Read();
+
+                passwordStr = msqlReader.GetString("passwrd");
+
+            }
+            catch (Exception er)
+            {
+                //Assert//.Show(er.Message);
+            }
+            finally
+            {
+                //always close the connection
+                msqlConnection.Close();
+            }
+
+            return passwordStr;
+        }
+        #endregion
+
+        #region Device
+
+
+        public static int DoRegisterNewDevice(StorageDeviceInfo DeviceDetails)
+        {
+            int returnVal = 0;
+            MySql.Data.MySqlClient.MySqlConnection msqlConnection = OpenDbConnection();
+
+            try
+            {
+                //define the command reference
+                MySql.Data.MySqlClient.MySqlCommand msqlCommand = new MySql.Data.MySqlClient.MySqlCommand();
+
+                //define the connection used by the command object
+                msqlCommand.Connection = msqlConnection;
+
+                msqlCommand.CommandText = "INSERT INTO Device(DeviceId,lastScannedDate,lastUsedHost,files,remark) "
+                                                   + "VALUES(@DeviceId,@lastScannedDate,@lastUsedHost,@files,@filesBackupHere,@remark)";
+
+                msqlCommand.Parameters.AddWithValue("@DeviceId", DeviceDetails.deviceId);
+                msqlCommand.Parameters.AddWithValue("@files", DeviceDetails.files);
+                msqlCommand.Parameters.AddWithValue("@lastScannedDate", DeviceDetails.lastScannedDate);
+                msqlCommand.Parameters.AddWithValue("@lastUsedHost", DeviceDetails.lastUsedHost);               
+
+                msqlCommand.ExecuteNonQuery();
+
+                returnVal = 1;
+            }
+            catch (Exception er)
+            {
+                returnVal = 0;
+            }
+            finally
+            {
+                //always close the connection
+                msqlConnection.Close();
+            }
+            return returnVal;
+        }
+
+
+        public static List<StorageDeviceInfo> GetAllDeviceList()
+        {
+            return QueryAllDeviceList();
+        }
+        private static List<StorageDeviceInfo> QueryAllDeviceList()
+        {
+            List<StorageDeviceInfo> DeviceList = new List<StorageDeviceInfo>();
+
+            MySql.Data.MySqlClient.MySqlConnection msqlConnection = OpenDbConnection();
+
+            try
+            {   //define the command reference
+                MySql.Data.MySqlClient.MySqlCommand msqlCommand = new MySql.Data.MySqlClient.MySqlCommand();
+                msqlCommand.Connection = msqlConnection;
+
+                msqlCommand.CommandText = "Select * From Device ;";
+                MySql.Data.MySqlClient.MySqlDataReader msqlReader = msqlCommand.ExecuteReader();
+
+                while (msqlReader.Read())
+                {
+                    StorageDeviceInfo Device = new StorageDeviceInfo();
+
+                    /*
+                    Device.deviceId = msqlReader.GetString("DeviceId");
+                    Device.lastScannedDate = msqlReader.GetString("lastScannedDate");
+                    Device.lastUsedHost = msqlReader.GetString("lastUsedHost");
+                    Device.lastUsedHost = msqlReader.GetString("lastUsedHost");
+                    Device.files = msqlReader.GetString("files");               
+                    */
+                    
+                    DeviceList.Add(Device);
+                }
+
+            }
+            catch (Exception er)
+            {
+            }
+            finally
+            {
+                //always close the connection
+                msqlConnection.Close();
+            }
+
+            return DeviceList;
+        }
+
+        #endregion
+
+
+
+    }
+}
